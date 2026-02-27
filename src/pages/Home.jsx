29879c6964e9bb7984a2a9931eb11d8dca024a0b
temitayo-kb <olawoyetemitayo@gmai.com>
2026-02-27 02:05:55 +0100

    Fix component import case sensitivity

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Common/Header/Header";
-import MainComponent from "../components/LandingPage/MainComponent/mainComponent";
+import MainComponent from "../components/LandingPage/MainComponent/MainComponent";
 
 function HomePage() {
   return (
